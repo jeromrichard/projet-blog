ce14958a5405618cb66dc85380fc06a147ac905e
jeromrichard <jeromerichard72@yahoo.com.com>
2020-08-19 16:55:20 +0200

    essai image

--- src/template/client-template.js
+++ src/template/client-template.js
@@ -5,18 +5,19 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
-const ComponentName = ({ data: {client: {data: {Titre_Article, Body}}} }) => {
+const ComponentName = ({ data: {client: {data: {Titre_Article, Body, image_détail}}} }) => {
     return (<div>
         <Layout>
             <div>
                 <SEO title="Page client"/>
                 <h1>Client</h1>
                         <p>{Titre_Article}</p>
+                        <img>{image_détail}</img>
                         <p>{Body}</p>
 
 
             </div>
-                <p><Link to="/page-2/">Go back to Collaborateur</Link></p>
+                <p><Link to="/page-2/">Go back to Site web</Link></p>
 
                 <p><Link to="/">Go back to the homepage</Link></p>
         </Layout>
