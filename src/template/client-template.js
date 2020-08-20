fefe73a709d183e5e84423491eadd9c4ae69a81c
jeromrichard <jeromerichard72@yahoo.com.com>
2020-08-20 18:42:10 +0200

    implement date 2

--- src/template/client-template.js
+++ src/template/client-template.js
@@ -33,7 +33,7 @@ export const query = graphql`
             data {
                Titre_Article
                Body
-               Date_de_creation(MM,DD,YYYY)
+               Date_de_creation(formatString: "MMMM DD, YYYY")
             }  
         }
     }
